a050dca2a7041127dd8d018b4765d3c83a7c811a
anubhavsahu1232-cmd <anubhavsahu1232@gmail.com>
2026-07-06 12:01:26 +0530

    Add Program11 - Variables

---
+++ 01-Core-Java/02-Variables-and-DataTypes/Program11_Variables.java
@@ -0,0 +1,18 @@
+public class Program11_Variables {
+
+    public static void main(String[] args) {
+
+        int age = 22;
+        String name = "Anubhav";
+        char grade = 'A';
+        double percentage = 85.5;
+        boolean isPlaced = false;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Grade : " + grade);
+        System.out.println("Percentage : " + percentage);
+        System.out.println("Placed : " + isPlaced);
+
+    }
+}
