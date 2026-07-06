2d7662dc8b986868bc36199642220adba3cd42d4
anubhavsahu1232-cmd <anubhavsahu1232@gmail.com>
2026-07-06 12:14:43 +0530

    Add Program15 - DataTypeRange

---
+++ 01-Core-Java/02-Variables-and-DataTypes/Program15_DataTypeRange.java
@@ -0,0 +1,13 @@
+public class Program15_DataTypeRange {
+
+    public static void main(String[] args) {
+
+        System.out.println("Byte Range    : " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println("Short Range   : " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("Integer Range : " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println("Long Range    : " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println("Float Range   : " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("Double Range  : " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+
+    }
+}
