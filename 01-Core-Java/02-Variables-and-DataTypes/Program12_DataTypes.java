e2a3964cf100e865f584b5918f4aa76674eda516
anubhavsahu1232-cmd <anubhavsahu1232@gmail.com>
2026-07-06 12:04:37 +0530

    Add Program12 - Data Types

---
+++ 01-Core-Java/02-Variables-and-DataTypes/Program12_DataTypes.java
@@ -0,0 +1,27 @@
+public class Program12_DataTypes {
+
+    public static void main(String[] args) {
+
+        byte age = 22;
+        short year = 2026;
+        int salary = 50000;
+        long population = 1400000000L;
+
+        float percentage = 85.5f;
+        double pi = 3.1415926535;
+
+        char grade = 'A';
+
+        boolean isJavaEasy = true;
+
+        System.out.println("Byte : " + age);
+        System.out.println("Short : " + year);
+        System.out.println("Int : " + salary);
+        System.out.println("Long : " + population);
+        System.out.println("Float : " + percentage);
+        System.out.println("Double : " + pi);
+        System.out.println("Char : " + grade);
+        System.out.println("Boolean : " + isJavaEasy);
+
+    }
+}
