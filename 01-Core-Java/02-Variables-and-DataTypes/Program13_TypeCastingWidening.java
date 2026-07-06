6e570df324e83ea78f9f4a7e3c9821cd54fc53a0
anubhavsahu1232-cmd <anubhavsahu1232@gmail.com>
2026-07-06 12:07:18 +0530

    Add Program13 - Widening Type Casting

---
+++ 01-Core-Java/02-Variables-and-DataTypes/Program13_TypeCastingWidening.java
@@ -0,0 +1,14 @@
+public class Program13_TypeCastingWidening {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        double value = number;
+
+        System.out.println("Integer Value : " + number);
+        System.out.println("Double Value  : " + value);
+
+    }
+
+}
