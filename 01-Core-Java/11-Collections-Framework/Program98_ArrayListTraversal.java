fb223fbc86ba3decd376ad634162ded353f9b5ca
anubhavsahu1232-cmd <anubhavsahu1232@gmail.com>
2026-07-15 20:31:54 +0530

    Complete ArrayList

---
+++ 01-Core-Java/11-Collections-Framework/Program98_ArrayListTraversal.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Program98_ArrayListTraversal {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> students = new ArrayList<>();
+
+        students.add("Anubhav");
+        students.add("Rahul");
+        students.add("Aman");
+        students.add("Rohit");
+
+        // 1. Normal For Loop
+        System.out.println("Using Normal For Loop:");
+
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i));
+        }
+
+        // 2. Enhanced For Loop
+        System.out.println("\nUsing Enhanced For Loop:");
+
+        for (String student : students) {
+            System.out.println(student);
+        }
+
+        // 3. Iterator
+        System.out.println("\nUsing Iterator:");
+
+        Iterator<String> iterator = students.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        // 4. ListIterator
+        System.out.println("\nUsing ListIterator:");
+
+        ListIterator<String> listIterator = students.listIterator();
+
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+    }
+}
